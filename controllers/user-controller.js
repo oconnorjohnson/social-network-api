aa3c5a204f8362eb4e8f585f8fa5b071d1814bc6
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-27 14:21:37 -0700

    added controllers/user-controller.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,49 @@
+const { User, THought, Thought } = require('../models')
+const user404Message = (id) => `User with ID: ${id} does not exist.`
+const user204Message = (id) => `User with ID: ${id} has been removed.`
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({ path: 'thoughts', select: '-__v'})
+        .populate({ path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(500).json(err))
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({ path: 'friends', select: '-__v'})
+        .populate({ path: 'thoughts', select: '-__v', populate: { path: 'reactions'}})
+        .select('-__v')
+        .then(dbUserData => dbUserData ? res.json(dbUserData) : res.status(404).json({ message: user404Message(params.id) }))
+        .catch(err => res.status(404).json(err))
+    },
+    createUser({ body }, res) {
+        User.create({ username: body.username, email: body.email})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err))
+    },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => dbUserData ? res.json(dbUserData) : res.status(404).json({ message: user404Message(params.id) }))
+        .catch(err => res.status(400).json(err))
+    },
+    deleteUser({ params }, res) {
+        User.findONeAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: user404Message(params.id) })
+            }
+            Thought.deleteMany({ username: dbUserData.username}).then(deletedData => deletedData ? res.json({ message: user204Message(params.id)}) : res.status(404).json({ message: user404Message(params.id) }))
+        })
+        .catch(err => res.status(400).json(err))
+    },
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId}, { $pull: { friends: params.friendId} })
+        .then(dbUserData => res.status(200).json(user204Message(params.frinedId, 'User')))
+        .catch(err => res.json(err))
+    }
+}
+
+module.exports = userController
